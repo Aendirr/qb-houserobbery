ce4353d3e19d9f6e3409c932dccdbf7ba5feaaa2
aendirr <118621878+Aendirr@users.noreply.github.com>
2024-04-14 22:41:38 +0300

    Update tr.lua

--- locales/tr.lua
+++ locales/tr.lua
@@ -1,21 +1,27 @@
 local Translations = {
     error = {
-        ["missing_something"] = "Birşeyleri kaçırıyormuşsun gibi görünüyor...",
+        ["missing_something"] = "Görünüşe göre bir şeyler eksik...",
         ["not_enough_police"] = "Yeterli polis yok..",
         ["door_open"] = "Kapı zaten açık..",
-        ["process_cancelled"] = "İşlem iptal edildi..",
+        ["process_cancelled"] = "İşlem İptal Edildi..",
         ["didnt_work"] = "İşe yaramadı..",
-        ["emty_box"] = "Kutu boş..",
+        ["emty_box"] = "Kutu Boş..",
+        ["not_allowed_time"] = "Bunu günün bu saatinde yapamazsınız."
     },
     success = {
         ["worked"] = "İşe yaradı!",
+    },
+    info = {
+        ["palert"] = "Ev Soygunu Girişimi",
+        ["henter"] = "~g~E~w~ - Girmek için",
+        ["hleave"] = "~g~E~w~ - Evden çıkmak için",
+        ["aint"] = "~g~E~w~ - ",
+        ["hsearch"] = "Aranıyor..",
+        ["hsempty"] = "Boş..",
     }
 }
 
-if GetConvar('qb_locale', 'en') == 'tr' then
-    Lang = Locale:new({
-        phrases = Translations,
-        warnOnMissing = true,
-        fallbackLang = Lang,
-    })
-end
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
